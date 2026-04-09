3ad005b8a64482aafcc63e05cb78d5d63673a30f
chicaizadiego55-tech <chicaizadiego55@gmail.com>
2026-04-08 22:02:21 -0500

    feat: reference css in html

---
+++ cazando.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("areaJuego");
+const contexto = canvas.getContext("2d");
+function graficarGato(){
+    let anchoGato = 100;
+    let altoGato = 100;
+    let x 
+
+}
